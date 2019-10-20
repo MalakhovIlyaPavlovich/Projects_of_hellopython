d6ee204163fc2f7c68f623fe89f175cd2a1895dc
MalakhovIlyaPavlovich <malakhov.ilya.pavlovich@yandex.ru>
2019-10-20 18:17:43 +0700

    Release

--- Project3/main.py
+++ Project3/main.py
@@ -1,9 +1,11 @@
 language = input('Choose language:\n1. English\n2. Russian\n')
 while True:
-    if language.lower() == 'english' or language.lower() == 'en' or language == '1' or language == '1.' or language.lower() == '1. english':
+    if (language.lower() == 'english' or language.lower() == 'en' or language == '1' or language == '1.' or
+        language.lower() == '1. english'):
         import local_en as loc
         break
-    elif language.lower() == 'russian' or language.lower() == 'ru' or language == '2' or language == '2.' or language.lower() == '2. russian':
+    elif (language.lower() == 'russian' or language.lower() == 'ru' or language == '2' or language == '2.' or
+          language.lower() == '2. russian'):
         import local_ru as loc
         break
     else: language = input('Choose language from the proposed:\n1. English\n2. Russian\n')
